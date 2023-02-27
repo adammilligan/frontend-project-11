44ab5de68bd75382befbb2f0f0f27f8b6d6e66e4
Вадим Валыгин <vadim12388@mail.ru>
2023-02-27 13:51:12 +0300

    end 1st step change

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
   devServer: {
     open: true,
